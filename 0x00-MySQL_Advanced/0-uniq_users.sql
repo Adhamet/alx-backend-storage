368961c8c6bfae22d766f038e76f0670609ff167
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2024-01-11 23:18:18 +0200

    unique users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,10 +1,7 @@
 -- Script that creates a table users
--- lol xd
--- haha
--- wtf
 
-CREATE TABLE IF NOT EXISTS users {
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
-};
+);
