b17775ff9e171267fd2f9133c3736e10bf265166
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2024-02-01 10:31:33 +0200

    best band ever! fix

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,6 +2,5 @@
 -- ordered by the number of (non-unique) fans
 SELECT origin, COUNT(fan_id) AS nb_fans
 FROM metal_bands
-JOIN fans ON metal_bands.band_id = fans.band_id
 GROUP by origin
 ORDER by nb_fans DESC;
