386a076a9365fd08b358b842e960d048ca81dacd
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2024-02-01 10:42:50 +0200

    added LIKE query

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,4 @@
--- Script that lists band with Glam Rock
--- As their main style, ranked by their
--- longevity.
+-- Script that lists all bands with Glam Rock as their main style, 
+-- ranked by longevity
 SELECT band_name, (IFNULL(split, 2022) - formed) As lifespan
-FROM metal_bands WHERE style = 'Glam Rock' ORDER by lifespan DESC;
+FROM metal_bands WHERE style = '%Glam Rock%' ORDER by lifespan DESC;
