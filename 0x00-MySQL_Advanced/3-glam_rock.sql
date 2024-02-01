c4ed1b45293b3bd8c12ecf36541bc694a305e3df
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2024-02-01 10:43:59 +0200

    fixed style to search for

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- Script that lists all bands with Glam Rock as their main style, 
 -- ranked by longevity
 SELECT band_name, (IFNULL(split, 2022) - formed) As lifespan
-FROM metal_bands WHERE style = '%Glam Rock%' ORDER by lifespan DESC;
+FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER by lifespan DESC;
