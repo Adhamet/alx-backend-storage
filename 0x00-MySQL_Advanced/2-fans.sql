511972cc78754ecefdab3d8127361cee3b779728
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2024-02-01 10:32:44 +0200

    changed logic count to sum

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- script that ranks country origins of bands,
 -- ordered by the number of (non-unique) fans
-SELECT origin, COUNT(fan_id) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP by origin
 ORDER by nb_fans DESC;
