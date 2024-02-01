bb5bc3fde5b9c39fc0e51077037d5ad178b671a4
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2024-02-01 12:12:45 +0200

    fixed syntax

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -3,7 +3,7 @@
 -- or returns 0 if the second number is equal to 0.
 DELIMITER //
 DROP function IF EXISTS SafeDiv;
-CREATE function SafeDiv (a INT, b INT) RETURNS FLOAT DETERMNISTIC
+CREATE function SafeDiv (a INT, b INT) RETURNS FLOAT DETERMINISTIC
 BEGIN
     return (IF (b = 0, 0, a / b));
 END;
