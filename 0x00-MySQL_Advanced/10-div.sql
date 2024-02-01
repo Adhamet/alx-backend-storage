73ee87b4ea80a955d02dffb086ff5e2d7b534996
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2024-02-01 12:11:23 +0200

    function divides two numbers

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,10 @@
+-- Create a function SafeDiv
+-- that divides (and returns) the first number by the second,
+-- or returns 0 if the second number is equal to 0.
+DELIMITER //
+DROP function IF EXISTS SafeDiv;
+CREATE function SafeDiv (a INT, b INT) RETURNS FLOAT DETERMNISTIC
+BEGIN
+    return (IF (b = 0, 0, a / b));
+END;
+// DELIMITER
